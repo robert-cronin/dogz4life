4a41f002342e8d8d5d51b628f74bc029944b8bb7
Robbie Cronin <robert.cronin@gatech.edu>
2020-11-05 19:53:19 +0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import DocumentTitle from 'react-document-title';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
+import Page4 from './components/Page4';
+import Footer from './components/Footer';
+import './static/style';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+  
+}
+interface AppState {
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props) {
+    super(props)
+
+  }
+
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div className="home-page">
+        <Header key="header" />
+        <Banner key="banner" />
+        <Page1 key="page1" />
+        <Page2 key="page2" />
+        <Page3 key="page3" />
+        <Page4 key="page4" />
+        <Footer key="footer" />
+        <DocumentTitle title="凤蝶 - 移动建站平台" />
+      </div>
+    );
+  }
 }
 
 export default App;
