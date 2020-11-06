600cb71fce53624d5aea7af01564041740f4120c
Robbie Cronin <robert.cronin@gatech.edu>
2020-11-06 22:10:19 +0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Banner from './components/Banner';
 import Navigation from './components/Navigation';
 import Logo from './static/dogz4lifelogo.jpg'
+import Bella from './static/bella.jpg'
+import Benni from './static/benni-silly.jpg'
 import DocumentTitle from 'react-document-title';
-import { Layout, BackTop } from 'antd';
+import { Layout, BackTop, Row, Col, Image, List } from 'antd';
 import { UpCircleOutlined } from '@ant-design/icons';
 const { Header, Footer, Content } = Layout;
 
@@ -30,24 +32,71 @@ class App extends React.Component<AppProps, AppState> {
       textAlign: 'center',
       fontSize: 30,
     };
+    const style1: React.CSSProperties = {
+      height: '1000px',
+    }
+    const threeSteps = [
+      "1. Book an apointment",
+      "2. Bring your puppy on the selected date",
+      "3. Come back when they're ready!"
+    ]
     return (
       <>
         <Layout>
           <Header>
             <Navigation />
           </Header>
+
           <Content>
-            <Banner backgroundImage={Logo} />
+            <Banner backgroundImage={Logo} odd />
+            <Banner even style={{ height: '100vmin' }}>
+              <Row gutter={[24, 0]} style={{ height: '100vmin' }} justify="space-around" align="middle">
+                <Col span={12} style={{
+                  height: '100vmin',
+                  backgroundImage: `url(${Bella})`,
+                  backgroundPosition: 'right',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'cover'
+                }} />
+                <Col span={12} style={{
+                  textAlign: 'center'
+                }}>
+                  <p className="text-5xl font-mono">Take care of your pet in 3 simple steps</p>
+                  <List
+                    dataSource={threeSteps}
+                    renderItem={item => (
+                      <List.Item>
+                        <p className="text-xl font-mono">{item}</p>
+                      </List.Item>
+                    )}
+                  />
+
+                </Col>
+              </Row>
+            </Banner>
+            <Banner odd>
+              <Row gutter={[24, 0]} style={{ height: '100vmin' }} justify="space-around" align="middle">
+                <Col span={12} style={{
+                  textAlign: 'center'
+                }}>
+                  <p className="text-4xl font-mono">We love your pets as much as you do!</p>
+                  <p className="text-2xl font-mono">Give your loved one the best treatment.</p>
+                </Col>
+                <Col span={12} style={{
+                  height: '100vmin',
+                  backgroundImage: `url(${Benni})`,
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'cover'
+                }} />
+              </Row>
+            </Banner>
             <Banner even>
             </Banner>
-            <Banner>
-            </Banner>
-            <Banner even>
-            </Banner>
-            <Banner>
+            <Banner odd>
             </Banner>
           </Content>
-          <Footer>Copyright ©2020 Dogz4Life LLC.</Footer>
+          <Footer>Copyright ©2020 Dogz4Life. Created by FortyTwoApps.</Footer>
         </Layout>
         <BackTop style={backTopStyle}>
           <UpCircleOutlined />
